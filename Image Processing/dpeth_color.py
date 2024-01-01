b8fdf401c5f68727e15b975001e09909acbfcffa
pops-95 <layekfdi@gmail.com>
2024-01-01 11:05:40 +0530

    with 1m as threshold

--- Image Processing/dpeth_color.py
+++ Image Processing/dpeth_color.py
@@ -5,19 +5,31 @@ import pyrealsense2 as rs
 
 img_name="E:\\share\\debu\\Cropped_Image\\Exposure_Image\\Color_Frame\\frame_32.jpg"
 file_name="E:\\share\\debu\\Cropped_Image\\Exposure_Image\\Depth_Frame\\frame_32.txt"
+thresh=1000
 
 img=cv.imread(img_name)
 img=cv.cvtColor(img,cv.COLOR_BGR2RGB)
 
 depth_data=np.loadtxt(file_name,dtype='float')
-print(depth_data.shape[0])
+print(depth_data[360,640])
 
 color_image=np.zeros((depth_data.shape[0],depth_data.shape[1],3),np.uint8)
 
 for i in range(depth_data.shape[0]):
     for j in range(depth_data.shape[1]):
         if(depth_data[i,j])==0:
-            color_image[i,j,0]=255
+            continue
+        else:
+            diff=depth_data[i,j]-thresh
+            diff=diff*100
+            if(diff==0):
+                color_image[i,j,0]=255
+            else:
+                map_value=(diff*127)/thresh
+                map_value=round(map_value)
+                color_image[i,j,0]=127+map_value
+                color_image[i,j,1]=127+map_value
+                color_image[i,j,2]=127+map_value
 
 
 
