d3e498a862d34716272c9726539cdc8c8b52d401
pops-95 <layekfdi@gmail.com>
2024-01-19 17:16:27 +0530

    plot number of pixel per distance wise

--- Image Processing/dpeth_color.py
+++ Image Processing/dpeth_color.py
@@ -7,26 +7,68 @@ img_name="E:\\share\\debu\\Cropped_Image\\Exposure_Image\\Color_Frame\\frame_32.
 file_name="E:\\share\\debu\\Cropped_Image\\Exposure_Image\\Depth_Frame\\frame_32.txt"
 thresh=1000
 
-img=cv.imread(img_name)
-img=cv.cvtColor(img,cv.COLOR_BGR2RGB)
+
+
+ending_dist=2000
+
+
+dist_value=[]
+number_pixel=[]
+
+
+
 
 depth_data=np.loadtxt(file_name,dtype='float')
+
+def plot_pixel():
+    starting_dist=800
+    pix_num=0
+    while(starting_dist!=ending_dist):
+        dist_value.append(str(starting_dist))
+        for i in range(depth_data.shape[0]):
+           for j in range(depth_data.shape[1]):
+               diff=depth_data[i,j]-starting_dist
+               if(-50<diff and  diff <50):
+                   pix_num=pix_num+1
+        
+        pix_num=pix_num/10000
+        number_pixel.append(pix_num)
+        pix_num=0
+        starting_dist=starting_dist+100
+
+
+
+# color_map=rs.colorizer()
+# img=cv.imread(img_name)
+# img=cv.cvtColor(img,cv.COLOR_BGR2RGB)
+
+
+
 print(depth_data[360,640])
 
-color_image=np.zeros((depth_data.shape[0],depth_data.shape[1],3),np.uint8)
-
-for i in range(depth_data.shape[0]):
-    for j in range(depth_data.shape[1]):
-        if(depth_data[i,j])==0:
-            continue
-        else:
-            diff=depth_data[i,j]-thresh
-            if(diff>0):
-                color_image[i,j,0]=img[i,j,0]
-                color_image[i,j,1]=img[i,j,1]
-                color_image[i,j,2]=img[i,j,2]
 
 
+# color_image=np.zeros((depth_data.shape[0],depth_data.shape[1],3),np.uint8)
 
-plt.imshow(color_image)
+# for i in range(depth_data.shape[0]):
+#     for j in range(depth_data.shape[1]):
+#         if(depth_data[i,j])==0:
+#             continue
+#         else:
+#             diff=depth_data[i,j]-thresh
+#             if(diff>0):
+#                 color_image[i,j,0]=img[i,j,0]
+#                 color_image[i,j,1]=img[i,j,1]
+#                 color_image[i,j,2]=img[i,j,2]
+
+
+
+# plt.imshow(color_image)
+# plt.show()
+plot_pixel()
+
+print(dist_value)
+print(number_pixel)
+
+plt.bar(dist_value,number_pixel)
 plt.show()
