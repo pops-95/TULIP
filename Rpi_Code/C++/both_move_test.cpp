2dd134a59196ff6f5ddbdbb59fd247fd5d1b17bb
pops.95 <layekfdi@gmail.com>
2024-04-12 13:21:43 +0530

    3 position accurately with modified errors

--- Rpi_Code/C++/both_move_test.cpp
+++ Rpi_Code/C++/both_move_test.cpp
@@ -20,11 +20,14 @@ int main(){
      int init_y=416;
      int zero_y=206;
     int camera_val_y=21;
-    int error_x=18;
-    int error_y=37;
-   
+    int error_x_posve=18;
+     int error_x_negve=9;
+    int error_y_posve=28;
+    int error_y_negve=45;
+   int error_y;
+   int error_x;
 
-    int camera_values[]={42,-18};
+    int camera_values[]={102,-152,-52,-98,-155,81};
     // int camera_values[]={4,-113};
     // move_x_front();
     move_x_back();
@@ -46,45 +49,66 @@ int main(){
 
     /////////////////////////////////////
 
-     int dis_x=init_x+error_x+camera_values[1];
-     int dis_y=init_y-zero_y+error_y-camera_values[0];
-     val.y_steps=(dis_y)*pulse_per_dis_y;
-     cout<<"Pulse required in y is = "<<val.y_steps<<"\n";
-     val.x_steps=(13.603*(dis_x))-2620.4;
-     cout<<"Pulse required in x is = "<<val.x_steps<<"\n";
-     val.Dirx=x_front;
-     val.Diry=y_right;
-      move_x(&val);
-    move_y(&val);
-    move_z(z_down,30); 
+//      int dis_x=init_x+error_x+camera_values[1];
+//      if(camera_values[0]<0){
+//           error_y=error_y_negve;
+//      }
+//      else{
+//           error_y=error_y_posve;
+//      }
+//      int dis_y=init_y+error_y-camera_values[0];
+//      // val.y_steps=(dis_y)*pulse_per_dis_y;
+//      val.y_steps=(int)(0.0018*dis_y*dis_y)+(21.315*(dis_y))-4544.1;
+//      cout<<"Pulse required in y is = "<<val.y_steps<<"\n";
+//      val.x_steps=(13.603*(dis_x))-2620.4;
+//      cout<<"Pulse required in x is = "<<val.x_steps<<"\n";
+//      val.Dirx=x_front;
+//      val.Diry=y_right;
+//       move_x(&val);
+//     move_y(&val);
+//     move_z(z_down,30); 
 
 
     /////////////////////////////////////
 
-//     for(int i=0;i<=2;i++){
-//          // Generate random number
-//         // double num = distribution(gen) * 10; // scale to range [-5, 5]
-//         move_z(z_up,40);
+    for(int i=0;i<=2;i++){
+         // Generate random number
+        // double num = distribution(gen) * 10; // scale to range [-5, 5]
+        move_z(z_up,40);
         
-//          int dis_x=init_x+error_x-camera_values[(i*2)+1];
-//          int dis_y=init_y+error_y-camera_values[i*2];
+   
+          if(camera_values[i*2]<0){
+          error_y=error_y_negve;
+     }
+     else{
+          error_y=error_y_posve;
+     }
+      if(camera_values[(i*2)+1]<0){
+          error_x=error_x_negve;
+     }
+     else{
+          error_x=error_x_posve;
+     }
+
+           int dis_x=init_x+error_x+camera_values[(i*2)+1];
+         int dis_y=init_y+error_y-camera_values[i*2];
         
-//         if(dis_x>prev_x_dis){
-//              val.Dirx=x_back;
-//         }
-//         else{
-//              val.Dirx=x_front;
-//         }
+        if(dis_x>prev_x_dis){
+             val.Dirx=x_front;
+        }
+        else{
+             val.Dirx=x_back;
+        }
 
-//          if(dis_y>prev_y_dis){
-//              val.Diry=y_right;
-//         }
-//         else{
-//              val.Diry=y_left;
-//         }
+         if(dis_y>prev_y_dis){
+             val.Diry=y_right;
+        }
+        else{
+             val.Diry=y_left;
+        }
 
-//         // int cal_pulse_x=(13.603*dis_x)-2620.4;
-//         // int cal_pulse_x=(13.741*(dis_x))-2679;
+        // int cal_pulse_x=(13.603*dis_x)-2620.4;
+        // int cal_pulse_x=(13.741*(dis_x))-2679;
 //          val.x_steps=(prev_x_dis-dis_x)*pulse_per_dis_x;
 //          if(val.x_steps<0){
 //             val.x_steps=val.x_steps*(-1);
@@ -95,22 +119,35 @@ int main(){
 //      if(val.y_steps<0){
 //             val.y_steps=val.y_steps*(-1);
 //          }
+     int req_y_pulse=(int)(0.0018*dis_y*dis_y)+(21.315*(dis_y))-4544.1;
+     val.y_steps=(req_y_pulse)-prev_y_pulse;
+           if(val.y_steps<0){
+            val.y_steps=val.y_steps*(-1);
+         }
+          cout<<"Pulse required in y is = "<<val.y_steps<<"\n";
+     int req_x_pulse=(13.603*(dis_x))-2620.4;
+          val.x_steps=(req_x_pulse)-prev_x_pulse;
+       
+         if(val.x_steps<0){
+            val.x_steps=val.x_steps*(-1);
+         }
+          cout<<"Pulse required in x is = "<<val.x_steps<<"\n";
 
-//     move_x(&val);
-//     move_y(&val);
+    move_x(&val);
+    move_y(&val);
 
-//     move_z(z_down,30); 
+    move_z(z_down,30); 
     
-//     usleep(move_delay);
-//      move_z(z_up,30);
+    usleep(move_delay);
+     move_z(z_up,30);
 
-//     prev_x_pulse=move_x_front();
-//     usleep(10000);
-//     prev_x_dis=0;
-//     // prev_x_pulse=val.x_steps;
-//     prev_y_dis=dis_y;
+    prev_x_pulse=req_x_pulse;
+    usleep(10000);
+    prev_x_dis=dis_x;
+    prev_y_pulse=req_y_pulse;
+    prev_y_dis=dis_y;
     
-//     }
+    }
 
         
 
