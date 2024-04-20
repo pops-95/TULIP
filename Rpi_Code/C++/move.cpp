579f94d8e7ef0ee3b96d982a0390418f13ee0760
pops-95 <layek@gmail.com>
2024-04-20 19:33:52 +0530

    fully written x and y distance measurement

--- Rpi_Code/C++/move.cpp
+++ Rpi_Code/C++/move.cpp
@@ -591,6 +591,88 @@ float MeasureDistance_Y(){
     
 
 void calculate_x_distance(Distances& dis){
+    int value_range=50;
+    float measured_dis[4][value_range];
+    float avg_measured_dis[value_range];
+    float distance;
+    int count=0;
+    float sum=0;
+    float actual_sum=0;
+    while (1)
+    {
+        MeasureDistance_X();
+        count++;
+
+        if(count>=40){
+            for(int i=0;i<4;i++){
+                for (int j=0;j<value_range;j++){
+                    measured_dis[i][j]=MeasureDistance_X();
+                }
+            }
+
+            for(int j=0;j<value_range;j++){
+                for(int i=0;i<4;i++){
+                    sum=sum+measured_dis[i][j];
+                }
+                avg_measured_dis[j]=sum/4;
+                sum=0;
+            }
+
+            for(int i=0;i<value_range;i++){
+                actual_sum=actual_sum+avg_measured_dis[i];
+            }
+            
+            // cout<<"Actual Distance = "<<actual_sum/value_range<<endl;
+            dis.x_distance=actual_sum/value_range;
+            actual_sum=0;
+
+        }
+        
+        
+    }
+    
+}
+
+void calculate_y_distance(Distances& dis){
+    int value_range=50;
+    float measured_dis[4][value_range];
+    float avg_measured_dis[value_range];
+    float distance;
+    int count=0;
+    float sum=0;
+    float actual_sum=0;
+    while (1)
+    {
+        MeasureDistance_Y();
+        count++;
+
+        if(count>=40){
+            for(int i=0;i<4;i++){
+                for (int j=0;j<value_range;j++){
+                    measured_dis[i][j]=MeasureDistance_Y();
+                }
+            }
+
+            for(int j=0;j<value_range;j++){
+                for(int i=0;i<4;i++){
+                    sum=sum+measured_dis[i][j];
+                }
+                avg_measured_dis[j]=sum/4;
+                sum=0;
+            }
+
+            for(int i=0;i<value_range;i++){
+                actual_sum=actual_sum+avg_measured_dis[i];
+            }
+            
+            // cout<<"Actual Distance = "<<actual_sum/value_range<<endl;
+            dis.y_distance=actual_sum/value_range;
+            actual_sum=0;
+
+        }
+        
+        
+    }
     
 }
 
