0e126c49102943f645783c51d4ebbcb85458d030
pops-95 <layek@gmail.com>
2024-06-04 00:47:50 +0530

    started function implementation

--- Python/GPIO.py
+++ Python/GPIO.py
@@ -14,6 +14,8 @@ class RPI_controller():
     def __init__(self,timing_budget=25000,inter_measurement_time=30):
         self.timing_budget=timing_budget
         self.inter_measurement_time=inter_measurement_time
+        self.roi_y=VL53L1X.VL53L1xUserRoi(6, 3, 9, 0)
+        self.roi_x=VL53L1X.VL53L1xUserRoi(6, 3, 9, 0)
         
     def gpio_init(self):
         GPIO.setmode(GPIO.BCM)
@@ -52,4 +54,25 @@ class RPI_controller():
             else:
                 break
     
-    
+    def move_x_front(self):
+        GPIO.output(Pinmap.dir_x,GPIO.LOW)
+        while(1):
+            if(GPIO.input(Pinmap.x_limit)==0):
+                self.pulse(Pinmap.pulse_x,50)
+            else:
+                # print("limit value {}".format(GPIO.input(Pinmap.x_limit)))
+                # if(GPIO.input(Pinmap.x_limit)==0):
+                #     print("limit value {}".format(GPIO.input(Pinmap.x_limit)))
+                #     continue
+                # else:
+                #     print("limit value {}".format(GPIO.input(Pinmap.x_limit)))
+                #     break
+                break
+            # break
+    def move_y_right(self):
+        GPIO.output(Pinmap.dir_y,GPIO.LOW)
+        while(1):
+            if(GPIO.input(Pinmap.y_limit_extra)==0):
+                self.pulse(Pinmap.pulse_y,100)
+            else:
+                break
